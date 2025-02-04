55a8d05f67866cd709b4187e3dc8a0939217a07e
akash <akash.tiwary@crio-users.in>
2025-02-04 20:19:48 +0000

    updated changes - 2025-02-04 20:19:48

--- assessment/cypress/e2e/spec.cy.js
+++ assessment/cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@
 describe('XNFT-Preview-Card-Component', () => {
   beforeEach(() => {
     // Visits the page before each test
-    cy.visit('http://127.0.0.1:5500/nft-preview-card-component-main/'); // Replace with your actual URL
+    cy.visit('http://localhost:8081/'); // Replace with your actual URL
   });
 
   it('should load the page and display the main elements', () => {
